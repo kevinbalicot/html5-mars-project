792761ce5926fd786870a940b7dcd2edb2838528
kevinfaurie <fauriekevinpro@gmail.com>
2018-10-02 15:22:12 +0200

    kevin

--- js/pilote.js
+++ js/pilote.js
@@ -9,3 +9,23 @@ function rudderLeftBtnClick(){
     rotation -= 30;
     document.getElementById("rudderImg").style.transform = "rotate("+rotation+"deg)";
 }
+
+function moveUp(){
+    let movementPowerValue = Number(document.getElementById("movementPower").value);
+    movementPowerValue += 0.25;
+    document.getElementById("movementPower").value = movementPowerValue;
+
+    if(movementPowerValue>1){
+        document.getElementById("movementPower").value = "1";
+    }
+}
+
+function moveDown(){
+
+let movementPowerValue = Number(document.getElementById("movementPower").value);
+movementPowerValue -= 0.25;
+document.getElementById("movementPower").value = movementPowerValue;
+
+if(movementPowerValue<0){
+    document.getElementById("movementPower").value = "0";
+}
